4bbd3eae1be33e3e7aed568757f6761acac94e2a
TaraPadilla <daalejito20@gmail.com>
2024-11-10 23:50:52 -0500

    Initial project setup

---
+++ src/main/java/com/proyectoingenieria/project_eduplay_rewards/ProjectEduplayRewardsApplication.java
@@ -0,0 +1,14 @@
+package com.proyectoingenieria.project_eduplay_rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEduplayRewardsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjectEduplayRewardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoingenieria/project_eduplay_rewards/ProjectEduplayRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoingenieria.project_eduplay_rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEduplayRewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
