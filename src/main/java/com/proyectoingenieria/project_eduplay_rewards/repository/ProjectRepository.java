5f47ce17839858357a8da2d60ded23b6a7afcd6e
TaraPadilla <daalejito20@gmail.com>
2024-11-11 14:08:08 -0500

    Entity Controller Projects

---
+++ src/main/java/com/proyectoingenieria/project_eduplay_rewards/repository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.proyectoingenieria.project_eduplay_rewards.repository;
+
+import com.proyectoingenieria.project_eduplay_rewards.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
+}
